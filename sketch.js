d9000db2b621ef2343b9900129a97a7123fece4c
ziv900308 <84578389+ziv900308@users.noreply.github.com>
2021-05-22 00:27:47 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://ziv900308.github.io/imageai/';
+let imageModelURL = 'https://ziv900308.github.io/AI/';
 
 // Video
 let video;
